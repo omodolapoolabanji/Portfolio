574e88300fc37aaf95185803cba5039195a56049
omodolapoolabanji <134759838+omodolapoolabanji@users.noreply.github.com>
2024-03-01 17:51:30 -0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Nav from './components/Nav';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Skills from './components/Skills'
+import Nav from './components/Nav.jsx';
+import Hero from './components/Hero.jsx';
+import Projects from './components/Projects.jsx';
+import Skills from './components/Skills.jsx'
 import '../styles/main.css'
-import Contact from './components/Contact';
-import Scroller from './components/scroller';
+import Contact from './components/Contact.jsx';
+import Scroller from './components/scroller.jsx';
 
 export default function App() {
     return (
